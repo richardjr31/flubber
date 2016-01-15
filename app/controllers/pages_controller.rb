4c144e194321019c41a54e6e51268361db0e6ffa
richardjr31 <richardwilliamsjr27@gmail.com>
2016-01-15 18:32:39 +0000

    Created Pages controller, Added pages index home profile explore pages.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+# The pages controller contains all the code of any page inside of /pages
+class PagesController < ApplicationController
+  
+  #back-end code for index
+  def index
+  end
+
+  #back-end code for home
+  def home
+  end
+
+  #back-end code for profile
+  def profile
+  end
+
+  #back-end code for exdplore
+  def explore
+  end
+  
+end
